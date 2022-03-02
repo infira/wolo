e3aac1fcdd139feef64a6f887fd9df9d8a1c3c64
Gen Taliar <gen@infira.ee>
2022-03-02 22:09:08 +0200

    Globals

---
+++ src/Globals/Globals.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Wolo\Globals;
+
+require_once '../../vendor/autoload.php';
+
+
+use Closure;
+use Wolo\Globals\GlobalsCollection;
+
+/**
+ * TempController
+ * @method static string name() Get collection name
+ * @method static bool exists(string $key) Does key exists
+ * @method static bool has(string $key) Does key exists
+ * @method static void set(string $key, mixed $value) Set new item
+ * @method static void add(mixed $value) Append new item
+ * @method static void append(mixed $value) Append new item
+ * @method static mixed get(string $key, mixed $default = null) Get item, if not found $returnOnNotFound will be returned
+ * @method static bool delete(string $key) delete item
+ * @method static array all() get all values
+ * @method static array tree() get all items and sub collections
+ * @method static void each(callable $callback) Call $callback for every item in current collection<br /> $callback($itemValue, $itemName)
+ * @method static void eachTree(callable $callback) Call $callback for every collection, sub collection and every item<br />$callback($itemValue, $itemName, $collectionName)
+ * @method static void eachCollection(callable $callback) Call $callback for every collection<br />$callback($Collection, $collectionName)
+ * @method static array collections() get this all collections
+ * @method static mixed magic(Closure $callback) Execute closure once per $key existence
+ * @method static mixed once(callable $callback, mixed ...$keys) Execute $callback once, if $keys is not provided then cache ID will be generated using callable footprint
+ * @method static mixed onceForce(string|array $key, bool $forceExec, callable $callback) Execute $callback once per $key existence or force it to call
+ * @method static bool flush() - Flush current data and collections
+ */
+class Globals
+{
+	private static array $collections = [];
+	
+	/**
+	 * @param string $key - collection name
+	 * @return GlobalsCollection
+	 */
+	public static function of(string $key): GlobalsCollection
+	{
+		if (!isset(self::$collections[$key])) {
+			self::$collections[$key] = new GlobalsCollection($key);
+		}
+		
+		return self::$collections[$key];
+	}
+	
+	public static function __callStatic(string $method, array $args)
+	{
+		return self::of('general')->$method(...$args);
+	}
+}
+
+debug(Globals::of('myCollection')->get);
---
+++ src/Globals/GlobalsCollection.php
@@ -0,0 +1,252 @@
+<?php
+
+namespace Wolo\Globals;
+
+
+use Closure;
+use Wolo\Str;
+use JetBrains\PhpStorm\ArrayShape;
+
+final class GlobalsCollection
+{
+	private array $collections = [];
+	
+	private array $data = [];
+	
+	private string $name;
+	
+	public function __construct(string $collectionName)
+	{
+		$this->name = $collectionName;
+	}
+	
+	/**
+	 * Generates new collection
+	 *
+	 * @param string $name - collection name
+	 * @return GlobalsCollection
+	 */
+	public function of(string $name): GlobalsCollection
+	{
+		if (!isset($this->collections[$name])) {
+			$this->collections[$name] = new GlobalsCollection($name);
+		}
+		
+		return $this->collections[$name];
+	}
+	
+	/**
+	 * Returns collection name
+	 *
+	 * @return string
+	 */
+	public function name(): string
+	{
+		return $this->name;
+	}
+	
+	/**
+	 * Checks if the item exists by key
+	 *
+	 * @param string $key
+	 * @return bool
+	 */
+	public function exists(string $key): bool
+	{
+		return array_key_exists($key, $this->data);
+	}
+	
+	/**
+	 * @see GlobalsCollection::exists()
+	 */
+	public function has(string $key): bool
+	{
+		return $this->exists($key);
+	}
+	
+	/**
+	 * Set new item
+	 *
+	 * @param string $key
+	 * @param mixed  $value
+	 * @return void
+	 */
+	public function set(string $key, mixed $value): void
+	{
+		$this->data[$key] = $value;
+	}
+	
+	/**
+	 * Add new item
+	 *
+	 * @param mixed $value
+	 * @return void
+	 */
+	public function add(mixed $value): void
+	{
+		$this->data[] = $value;
+	}
+	
+	/**
+	 * @see GlobalsCollection::add()
+	 */
+	public function append(mixed $value): void
+	{
+		$this->data[] = $value;
+	}
+	
+	/**
+	 * Get item, if not found $returnOnNotFound will be returned
+	 *
+	 * @param string $key
+	 * @param mixed  $default - if not found then that is returned
+	 * @return mixed/bool
+	 */
+	public function get(string $key, mixed $default = null): mixed
+	{
+		if (!$this->exists($key)) {
+			return $default;
+		}
+		
+		return $this->data[$key];
+	}
+	
+	/**
+	 * delete bye key
+	 *
+	 * @param string $key
+	 * @return bool
+	 */
+	public function delete(string $key): bool
+	{
+		if ($this->exists($key)) {
+			unset($this->data[$key]);
+		}
+		
+		return true;
+	}
+	
+	/**
+	 * get all items
+	 *
+	 * @return array
+	 */
+	public function all(): array
+	{
+		return $this->data;
+	}
+	
+	/**
+	 * Get current collection tree
+	 * [
+	 *      [collections] => [
+	 *          [collection1] => [
+	 *              [collections] => [...]
+	 *              [items] => [...]
+	 *          ]
+	 *      ]
+	 *      [items] => [
+	 *          "item1"=>"item value",
+	 *          "item2"=>"item2 value"
+	 *      ]
+	 *      .....
+	 */
+	#[ArrayShape(['collections' => "array", 'items' => "array"])] public function tree(): array
+	{
+		$data = ['collections' => []];
+		$this->eachCollection(function ($Collection, $collectionName) use (&$data)
+		{
+			$data['collections'][$collectionName] = ['collections' => [], 'items' => $Collection->getItems()];
+			$tree                                 = $Collection->getTree();
+			if ($tree['collections']) {
+				$data['collections'][$collectionName]['collections'] = $tree['collections'];
+			}
+		});
+		$data['items'] = $this->all();
+		
+		return $data;
+	}
+	
+	/**
+	 * Call $callback for every item in current collection<br />$callback($itemValue,$itemName)
+	 *
+	 * @param callable $callback
+	 * @return void
+	 */
+	public function each(callable $callback): void
+	{
+		foreach ($this->data as $key => $value) {
+			call_user_func_array($callback, [$value, $key]);
+		}
+	}
+	
+	/**
+	 * Call $callback for every collection, sub collection and every item<br />$callback($itemValue,$itemName,$collectionName)
+	 *
+	 * @param callable $callback
+	 * @return void
+	 */
+	public function eachTree(callable $callback): void
+	{
+		foreach ($this->data as $name => $value) {
+			call_user_func_array($callback, [$value, $name, $this->name]);
+		}
+		foreach ($this->collections as $name => $Collection) {
+			$Collection->eachTree($callback);
+		}
+	}
+	
+	/**
+	 * Call $callback for every collection<br />$callback($Collection,$collectionName)
+	 *
+	 * @param callable $callback
+	 * @return void
+	 */
+	public function eachCollection(callable $callback): void
+	{
+		foreach ($this->collections as $name => $Collection) {
+			call_user_func_array($callback, [$Collection, $name]);
+		}
+	}
+	
+	/**
+	 * get this all collections
+	 *
+	 * @return array
+	 */
+	public function collections(): array
+	{
+		return $this->collections;
+	}
+	
+	/**
+	 * Execute $callback once
+	 * if $keys is not provided then cache ID will be generated using callable footprint
+	 *
+	 * @param callable $callback method result will be set to memory for later use
+	 * @param mixed    ...$keys
+	 * @return mixed - $callback result
+	 */
+	public function once(callable $callback, ...$keys): mixed
+	{
+		$cid = hash("crc32b", Str::hashable($keys));
+		if (!$this->exists($cid)) {
+			$this->set($cid, $callback());
+		}
+		
+		return $this->get($cid);
+	}
+	
+	/**
+	 * Flush current data and collections
+	 *
+	 * @return bool
+	 */
+	public function flush(): bool
+	{
+		$this->collections = [];
+		$this->data        = [];
+		
+		return true;
+	}
+}
