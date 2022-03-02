a67b6f90a663aada59dc899c761a6bdff09ad091
Gen Taliar <gen@infira.ee>
2022-03-02 18:31:19 +0200

    str vars change

--- src/Str.php
+++ src/Str.php
@@ -15,7 +15,7 @@ class Str
 	public static function vars(string $string, array $vars, string $syntax = '{}'): string
 	{
 		$start = $syntax[0];
-		$end   = $syntax[1];
+		$end   = $syntax[1] ?? $syntax[0];
 		foreach ($vars as $name => $value) {
 			$string = str_replace([$start . $name . $end], $value, $string);
 		}
