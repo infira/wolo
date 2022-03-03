339a3861c2f2b9d258371be9d772a000e1c8f94b
Gen Taliar <gen@infira.ee>
2022-03-03 02:12:07 +0200

    docs change

--- src/Globals/Globals.php
+++ src/Globals/Globals.php
@@ -22,7 +22,7 @@ use Wolo\Globals\GlobalsCollection;
  * @method static void eachCollection(callable $callback) Call $callback for every collection<br />$callback($Collection, $collectionName)
  * @method static array collections() get this all collections
  * @method static mixed magic(Closure $callback) Execute closure once per $key existence
- * @method static mixed once(mixed ...$keys, callable $callback) Execute $callback once, if $keys is not provided then cache ID will be generated using callable footprint
+ * @method static mixed once(mixed|callable ...$keys) Execute $callback once, if $keys is not provided then cache ID will be generated using callable footprint
  * @method static mixed onceForce(string|array $key, bool $forceExec, callable $callback) Execute $callback once per $key existence or force it to call
  * @method static bool flush() - Flush current data and collections
  */
