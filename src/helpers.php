7798ff7fb37aa52e2225378d268fa7f194615b2e
Gen Taliar <gen@infira.ee>
2022-03-15 19:17:52 +0200

    get trace helper

--- src/helpers.php
+++ src/helpers.php
@@ -10,6 +10,24 @@ if (!function_exists('debug')) {
 	}
 }
 
+if (!function_exists('debug')) {
+	function getTrace(int $startAt = 0): string
+	{
+		$backTrace = debug_backtrace();
+		$until     = 15;
+		$trace     = [];
+		$nr        = 1;
+		for ($i = $startAt; $i <= $until; $i++) {
+			if (isset($backTrace[$i]['file'])) {
+				$trace[$nr] = $backTrace[$i]['file'] . ' : ' . $backTrace[$i]['line'];
+				$nr++;
+			}
+		}
+		
+		return str_replace(getcwd(), "", $trace);
+	}
+}
+
 
 if (!function_exists('checkArray')) {
 	function checkArray($array): bool
