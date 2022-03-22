f9935001fb69c8b69ad03840974a3b37a588fe3a
Gen Taliar <gen@infira.ee>
2022-03-22 10:34:32 +0200

    reflection helpers

---
+++ src/Reflection/Reflection.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Wolo\Reflection;
+
+class Reflection
+{
+    /**
+     * get class traits
+     *
+     * @param string|object $objectOrClass
+     * @param int|null      $depth - check also parents traits, 0 all teh way to to last parent
+     * @return \ReflectionClass[]
+     */
+    public static function getClassTraits(string|object $objectOrClass, int $depth = 0): array
+    {
+        $class  = new \ReflectionClass($objectOrClass);
+        $traits = $class->getTraits();
+        if ($depth !== null) {
+            $currentDepth = 0;
+            $depth        = $depth === 0 ? 99 : $depth;
+            while ($parent = $class->getParentClass() and $currentDepth <= $depth) {
+                $traits = array_merge($traits, $parent->getTraits());
+                $class  = $parent;
+                $currentDepth++;
+            }
+        }
+
+        return $traits;
+    }
+
+    /**
+     * Does class has trait
+     *
+     * @param string|object $objectOrClass
+     * @param string|object $findObjectOrClass
+     * @param bool          $checkParents
+     * @return bool
+     */
+    public static function classHasTrait(string|object $objectOrClass, string|object $findObjectOrClass, bool $checkParents = true)
+    {
+        $findClass = is_object($findObjectOrClass) ? $findObjectOrClass::class : $findObjectOrClass;
+        $traits    = self::getClassTraits($objectOrClass, $checkParents ? 0 : null);
+        foreach ($traits as $trait) {
+            if ($trait->getName() == $findClass) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
