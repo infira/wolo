106cf9d4ccfa11bb31de02db5e9fc6eb2a880321
Gen Taliaru <gen@infira.ee>
2022-10-27 12:47:17 +0300

    improved docs

--- src/Globals/Globals.php
+++ src/Globals/Globals.php
@@ -6,22 +6,23 @@ use Closure;
 
 /**
  * TempController
- * @method static string name() Get collection name
- * @method static bool exists(string $key) Does key exists
- * @method static bool has(string $key) Does key exists
- * @method static void set(string $key, mixed $value) Set new item
- * @method static void add(mixed $value) Append new item
- * @method static void append(mixed $value) Append new item
- * @method static mixed get(string $key, mixed $default = null) Get item, if not found $returnOnNotFound will be returned
- * @method static bool delete(string $key) delete item
- * @method static array all() get all values
- * @method static array tree() get all items and sub collections
- * @method static void each(callable $callback) Call $callback for every item in current collection<br /> $callback($itemValue, $itemName)
- * @method static void eachTree(callable $callback) Call $callback for every collection, sub collection and every item<br />$callback($itemValue, $itemName, $collectionName)
- * @method static void eachCollection(callable $callback) Call $callback for every collection<br />$callback($Collection, $collectionName)
- * @method static array collections() get this all collections
- * @method static mixed once(mixed ...$keys, callable $callback) Execute $callback once by hash-sum of $parameters
- * @method static bool flush() - Flush current data and collections
+ * @method static string name()
+ * @method static bool exists(string $key)
+ * @method static bool has(string $key)
+ * @method static void set(string $key, mixed $value)
+ * @method static void add(mixed $value)
+ * @method static void append(mixed $value)
+ * @method static mixed get(string $key, mixed $default = null)
+ * @method static bool delete(string $key)
+ * @method static array all()
+ * @method static array tree()
+ * @method static void each(callable $callback)
+ * @method static void eachTree(callable $callback)
+ * @method static void eachCollection(callable $callback)
+ * @method static array collections()
+ * @method static mixed once(mixed ...$keys)
+ * @method static bool flush()
+ * @see GlobalsCollection
  */
 class Globals
 {
@@ -33,7 +34,7 @@ class Globals
      */
     public static function of(string $key): GlobalsCollection
     {
-        if (!isset(self::$collections[$key])) {
+        if(!isset(self::$collections[$key])) {
             self::$collections[$key] = new GlobalsCollection($key);
         }
 
