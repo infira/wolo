23b44b00a878b0f6d0b74019a29555985a0fb3bb
Gen Taliaru <gen@infira.ee>
2022-10-30 23:06:51 +0200

    bade code removal

--- src/VarDumper.php
+++ src/VarDumper.php
@@ -9,8 +9,8 @@ class VarDumper
 {
     private static function getHeader(string $name): ?string
     {
-        foreach (getallheaders() as $header => $value) {
-            if (strtolower($header) === strtolower($name)) {
+        foreach(getallheaders() as $header => $value) {
+            if(strtolower($header) === strtolower($name)) {
                 return $value;
             }
         }
@@ -20,7 +20,7 @@ class VarDumper
 
     private static function isConsole(): bool
     {
-        if (!defined('PHP_SAPI')) {
+        if(!defined('PHP_SAPI')) {
             return false;
         }
 
@@ -29,20 +29,12 @@ class VarDumper
 
     public static function debug(...$vars): void
     {
-        if (count($vars) === 3 && is_string($vars[0]) && $vars[1] === '=>') {
-            self::debug([$vars[0] => $vars[2]]);
-        }
-        elseif (count($vars) === 2 && is_string($vars[0]) && str_ends_with($vars[0], ':=>')) {
-            self::debug([substr($vars[0], 0, -3) => $vars[1]]);
-        }
-        else {
-            foreach ($vars as $var) {
-                if (self::isConsole()) {
-                    echo self::console($var);
-                }
-                else {
-                    echo self::pre($var);
-                }
+        foreach($vars as $var) {
+            if(self::isConsole()) {
+                echo self::console($var);
+            }
+            else {
+                echo self::pre($var);
             }
         }
     }
@@ -65,7 +57,7 @@ class VarDumper
      */
     public static function dump(mixed $var): string
     {
-        if (is_array($var) || is_object($var)) {
+        if(is_array($var) || is_object($var)) {
             return print_r($var, true);
         }
         ob_start();
