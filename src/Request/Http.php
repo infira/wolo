b0dc5741bd636c935bdd8d47aaf929243be65b85
Gen Taliaru <gen@infira.ee>
2023-08-09 12:47:18 +0300

    code formatting

--- src/Request/Http.php
+++ src/Request/Http.php
@@ -257,22 +257,22 @@ class Http extends InstanceShortcuts
         if (isset($_SERVER['HTTP_CLIENT_IP'])) {
             $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
         }
-        elseif (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        else if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
             $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
         }
-        elseif (isset($_SERVER['HTTP_X_FORWARDED'])) {
+        else if (isset($_SERVER['HTTP_X_FORWARDED'])) {
             $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
         }
-        elseif (isset($_SERVER['HTTP_X_CLUSTER_CLIENT_IP'])) {
+        else if (isset($_SERVER['HTTP_X_CLUSTER_CLIENT_IP'])) {
             $ipaddress = $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
         }
-        elseif (isset($_SERVER['HTTP_FORWARDED_FOR'])) {
+        else if (isset($_SERVER['HTTP_FORWARDED_FOR'])) {
             $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
         }
-        elseif (isset($_SERVER['HTTP_FORWARDED'])) {
+        else if (isset($_SERVER['HTTP_FORWARDED'])) {
             $ipaddress = $_SERVER['HTTP_FORWARDED'];
         }
-        elseif (isset($_SERVER['REMOTE_ADDR'])) {
+        else if (isset($_SERVER['REMOTE_ADDR'])) {
             $ipaddress = $_SERVER['REMOTE_ADDR'];
         }
         else {
